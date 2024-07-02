df5e537857865e52e899dc9521f2362ed288b528
rmalegre <bestsoport@gmail.com>
2024-07-02 00:04:25 -0300

    auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>{children}</div>
+    <div className="h-full flex items-center justify-center mt-5">{children}</div>
   )
 }
 
